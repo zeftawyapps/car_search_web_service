9740a55c6b0029054603692de3053cd952290d5d
HP <moaz.salaheddin>
2024-05-19 22:49:46 +0300

    5 real

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -23,14 +23,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "8e1d224ede60fded2ee39cd777bd842f",
+"flutter_bootstrap.js": "88ec52090a13a5e632fe1754eb8f2c51",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "0b1ba628d426005ecfcb45322d6e63ee",
 "/": "0b1ba628d426005ecfcb45322d6e63ee",
-"main.dart.js": "68b9026f63c6c020956b0814a006b034",
+"main.dart.js": "db87a8e9539714c24cd071c04b3a8e87",
 "manifest.json": "ff78a8afb2816e62420d4d47b44d8d46",
 "version.json": "0e19c103eaa3f7cd0bae3db8dc49524e"};
 // The application shell files that are downloaded before a service worker can
